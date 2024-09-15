522e06d234998a15a61c1433b81d52bf78c0f496
sameerkumar2704 <shivamjnv01@gmail.com>
2024-09-15 19:46:02 +0530

    sign-up-in

--- client/public/index.js
+++ client/public/index.js
@@ -1,17 +1,52 @@
-import { CustomButton } from "./components/resuable_components.js"
-function createResuable( topics , custom_class , component , parent){
-    topics.forEach(element => {
-        const button = component( custom_class  , element  , './instruction/instruction.html')
-        const li = document.createElement("li")
-        li.appendChild(button)
-        parent.append(li)
-    });
-}
-function init(){
-    const quiz_top_container = document.getElementById('select-quciz-topic')
-    const topics = ["Computer Networks 🛜" , "Operating System 💻" , "Javascipt "]
-    createResuable(topics , "topics" , CustomButton , quiz_top_container)
+function init() {
+    const sign_in = document.getElementById('sign-in')
+    const sign_up = document.getElementById('sign-up')
+    const swith_to_sign_in_btn = document.getElementById('switch-to-sign-in')
+    const swith_to_sign_up_btn = document.getElementById('switch-to-sign-up')
+    const sign_in_password = document.getElementById('sign-in-input-box')
+    const show_password_sing_in = document.getElementById('show-password')
+    const show_password_sign_up = document.getElementById('show-password-sign-up')
+    const sign_up_password = document.getElementById('sign-up-password')
+    const sign_up_conform_password = document.getElementById('sign-up-conform-password')
+    const show_conform_password_sing_up = document.getElementById('show-conform-password')
 
-    
+
+    show_password_sing_in.addEventListener('change', (e) => {
+        if (e.target.checked) {
+            sign_in_password.type = "text"
+        } else {
+            sign_in_password.type = "password"
+        }
+    })
+    show_password_sign_up.addEventListener('change' , (e)=>{
+        if (e.target.checked) {
+            sign_up_password.type = "text"
+        } else {
+            sign_up_password.type = "password"
+        }
+    })
+    show_conform_password_sing_up.addEventListener('change' , (e)=>{
+        if (e.target.checked) {
+            sign_up_conform_password.type = "text"
+        } else {
+            sign_up_conform_password.type = "password"
+        }
+    })
+
+
+
+    swith_to_sign_in_btn.addEventListener('click', () => {
+        sign_in.style.display = "block"
+        sign_up.style.display = "none"
+
+
+    })
+    swith_to_sign_up_btn.addEventListener("click", () => {
+        sign_up.style.display = "block"
+        sign_in.style.display = "none"
+
+    })
+
+
 }
 init()
--- client/public/question/question.js
+++ client/public/question/question.js
@@ -1,6 +1,6 @@
 const question = [{
-  title:"Which statement is not correct about “init” process in Unix?",
-  options:[
+  title: "Which statement is not correct about “init” process in Unix?",
+  options: [
     "It is generally the parent of the login shell.",
     "It has PID 1.",
     "It is the first process in the system.",
@@ -8,11 +8,11 @@ const question = [{
 
 
   ],
-  correct_options:3
+  correct_options: 3
 },
 {
-  title:"Which statement is not correct about “init” process in Unix? -2",
-  options:[
+  title: "Which statement is not correct about “init” process in Unix? -2",
+  options: [
     "It is generally the parent of the login shell.",
     "It has PID 1.",
     "It is the first process in the system.",
@@ -20,39 +20,46 @@ const question = [{
 
 
   ],
-  correct_options:3
+  correct_options: 3
 }
 
 ]
+let test_completed_user = {
+  os:["12456"]
+}
 
 // maintaning recored of question 
-function questionSet(data){
+function questionSet(data) {
+  
   return {
-    question_list : data,
-    curr_index:0,
-    increment_index:function (){
+    question_list: data,
+    curr_index: 0,
+    increment_index: function () {
       this.curr_index++
     },
-    is_last_question: function(){ return this.question_list.length-1 === this.curr_index},
-    is_end: function(){ return this.question_list.length === this.curr_index}
+    is_last_question: function () { return this.question_list.length - 1 === this.curr_index },
+    is_end: function () { return this.question_list.length === this.curr_index }
   }
 }
 
 history.replaceState(null, null, 'http://localhost:8080/question/question.html')
 
-
+if (sessionStorage.getItem("refresh-count")) alert('warning your test will be submiited')
 window.addEventListener('beforeunload', (event) => {
   // Custom message
   console.log("done")
   // Standard for most browsers
   localStorage.setItem("test", "completed")
+  if (!sessionStorage.getItem("refresh-count")) sessionStorage.setItem("refresh-count", 1);
+
+
   // Some browsers may use this as the message
 
   // Return the message for older browsers
 });
 function preventBack() {
   console.log("hogaya")
-  
+
 }
 
 window.onload = preventBack;
@@ -63,32 +70,24 @@ function init() {
   const option_arr = document.querySelectorAll(".option");
   const next_question = document.getElementById('next-question')
   const question_title = document.getElementById('question-title')
-  const questions_set = new questionSet(question);
+  let questions_set = undefined;
+
+  window.addEventListener('beforeunload' , ()=>{
+    localStorage.
+  })
   refreshQuestion()
-  function next_btn_handler(){
+  function next_btn_handler() {
     questions_set.increment_index()
-    if(questions_set.is_end()){
+    sessionStorage.setItem("question-status", JSON.stringify(questions_set))
+    if (questions_set.is_end()) {
       window.location.href = "https://www.amazon.in/"
     }
- 
-    
+
+
     refreshQuestion()
   }
-  function refreshQuestion(){
-    const current_question = questions_set.curr_index;
-    question_title.innerHTML = questions_set.question_list[current_question].title
-    option_arr.forEach((option , index) => {
-      option.classList.remove("question-option-active")
-      option.classList.add("question-option-non-active")
-      option.innerHTML = questions_set.question_list[current_question].options[index]
-    })
-    next_question.disabled = true;
-    console.log(questions_set.is_last_question())
-    if(questions_set.is_last_question()){
-      next_question.innerHTML = "submit"
-    }
 
-  }
+
   function option_click_handler() {
 
     // it uses object of current elment
@@ -102,11 +101,37 @@ function init() {
     next_question.disabled = false
   }
 
-  option_arr.forEach((option) => {
-    option.addEventListener('click', option_click_handler)
 
-  })
-  next_question.addEventListener('click' ,next_btn_handler )
+  function refreshQuestion() {
+    if (sessionStorage.getItem('question-status')) {
+      questions_set = JSON.parse(sessionStorage.getItem("question-status"))
+    } else {
+      questions_set = new questionSet(question);
+
+    }
+    console.log(questions_set)
+    const current_question = questions_set.curr_index;
+    question_title.innerHTML = questions_set.question_list[current_question].title
+    option_arr.forEach((option, index) => {
+      option.classList.remove("question-option-active")
+      option.classList.add("question-option-non-active")
+      option.innerHTML = questions_set.question_list[current_question].options[index]
+    })
+    next_question.disabled = true;
+    console.log(questions_set.is_last_question())
+    if (questions_set.is_last_question()) {
+      next_question.innerHTML = "submit"
+    }
+    option_arr.forEach((option) => {
+      option.addEventListener('click', option_click_handler)
+
+    })
+    next_question.addEventListener('click', next_btn_handler)
+
+
+  }
+
+
 
 }
 init()
