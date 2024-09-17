e2176cb99b6d53636e2bf134769c0e2519ab8b65
sameerkumar2704 <shivamjnv01@gmail.com>
2024-09-17 18:39:45 +0530

    question-layout

--- client/public/test_type/test_type.js
+++ client/public/test_type/test_type.js
@@ -1,16 +1,13 @@
-import { CustomButton } from "../components/resuable_components.js"
-function createResuable( topics , custom_class , component , parent){
-    topics.forEach(element => {
-        const button = component( custom_class  , element  , './instruction/instruction.html')
-        const li = document.createElement("li")
-        li.appendChild(button)
-        parent.append(li)
-    });
-}
+
 function init(){
-    const quiz_top_container = document.getElementById('select-quciz-topic')
-    const topics = ["Computer Networks 🛜" , "Operating System 💻" , "Javascipt "]
-    createResuable(topics , "topics" , CustomButton , quiz_top_container)
+    const template_list = document.getElementsByClassName('test-template');
+   console.log(template_list.length)
+    Array.from(template_list).forEach((element , index)=>{
+        element.addEventListener('click' , (e)=>{
+            sessionStorage.setItem('test-type' , index)
+            window.location.href = "http://localhost:8080/question/question.html"
+        })
+    })
     const logined_user = localStorage.getItem('login_id')
     
     if(!logined_user) window.location.replace( 'http://localhost:8080/')
